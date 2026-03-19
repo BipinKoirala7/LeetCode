d5855d111f4d05b8327d42ef01efc1270bda55e6
Bipin Koirala <bipinkoirala2061@gmail.com>
2026-03-19 16:07:43 +0545

    Graph

---
+++ Practice/Graphs/Main.java
@@ -0,0 +1,11 @@
+package Practice.Graphs;
+
+public class Main {
+  static void main() {
+    Graph graph = new Graph();
+
+    System.out.println("Add Vertex: " + graph.addVertex("A"));
+
+    graph.printGraph();
+  }
+}
