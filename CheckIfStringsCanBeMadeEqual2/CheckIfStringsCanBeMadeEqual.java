c5a2f296d74211903818323b2fe0a9c5524df5f4
Bipin Koirala <bipinkoirala2061@gmail.com>
2026-03-30 08:47:23 +0545

    Check If Strings Can Be Made Equal ||

---
+++ CheckIfStringsCanBeMadeEqual2/CheckIfStringsCanBeMadeEqual.java
@@ -0,0 +1,15 @@
+package CheckIfStringsCanBeMadeEqual2;
+
+public class CheckIfStringsCanBeMadeEqual {
+  public boolean checkStrings(String s1, String s2) {
+    int counter = 0;
+    for(int i = 0; i < s1.length(); i = i + 2){
+      int j = i + 2;
+      if(j > s1.length()){
+
+      }
+    }
+
+    return false;
+  }
+}
---
+++ CheckIfStringsCanBeMadeEqual2/Main.java
@@ -0,0 +1,8 @@
+package CheckIfStringsCanBeMadeEqual2;
+
+public class Main {
+  static void main() {
+    CheckIfStringsCanBeMadeEqual canBeMadeEqual = new CheckIfStringsCanBeMadeEqual();
+    System.out.println("Can Be Equal: " + canBeMadeEqual.checkStrings("abcdba", "cabdab"));
+  }
+}
