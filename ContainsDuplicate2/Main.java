8507b7f156940393cb295278a62ca8d7fe43c7d5
Bipin Koirala <bipinkoirala2061@gmail.com>
2026-04-02 09:20:27 +0545

    Contains Duplicate ||

---
+++ ContainsDuplicate2/Main.java
@@ -0,0 +1,8 @@
+package ContainsDuplicate2;
+
+public class Main {
+  static void main() {
+    ContainsDuplicate duplicate = new ContainsDuplicate();
+    System.out.println(duplicate.containsNearbyDuplicate(new int[]{1, 2, 3, 1}, 3));
+  }
+}
